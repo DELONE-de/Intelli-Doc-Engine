37a20f7d16fc1a20ab720836ceeb765703576bd8
convenati <convenati@gmail.com>
2026-03-14 20:36:26 +0000

    Add dotenv support for CDK environment config

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import * as cdk from 'aws-cdk-lib';
 import { MyFirstStack } from '../lib/my-first-stack';
 
 const app = new cdk.App();
-new MyFirstStack(app, 'MyFirstStack');
+new MyFirstStack(app, 'MyFirstStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
